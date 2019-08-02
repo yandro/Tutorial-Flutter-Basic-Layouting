a21c711ef82e090c126ad64cb0b81c7b88ba8510
~anang <anang120799@gmail.com>
2019-08-02 08:29:03 +0700

    comment message

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
+//main di eksekusi saat aplikasi pertama kali di jalankan
 void main() => runApp(MyApp(tittle: "dasds",));
 
+//MyApp adalah kelas yang pertama kali di jalankan
+//sesuai dengan yang sudah di deklarasikan di "main => runApp"
 class MyApp extends StatelessWidget {
+  //constructor kelas
   const MyApp({Key key, this.tittle}) : super(key: key);
+  //parameter di deklarasikan dengan final dan di masukkan ke constructor
   final String tittle;
+  
+  //widget build merupakan method yang pertama kali
+  //dijalankan pada kelas ini
+  //seperti  onCreate pada android studio
   @override
   Widget build(BuildContext context) {
+    //Material app merupakan setting pada seluruh aplikasi
     return MaterialApp(
+      //home perupakan tampilan utama, disini tampilan utamanya adalah MyLoginPage
       home: MyLoginPage(title: "Login",),
+      //ThemeData berisi tema aplikasi seperti warna toolbar dsb
       theme: ThemeData(
         primarySwatch: Colors.pink
       ),
@@ -16,16 +28,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//MyLoginPage ini adalah statefulWidget artinya
+//UI bisa berubah jika statenya berubah
 class MyLoginPage extends StatefulWidget {
   MyLoginPage({Key key, this.title}) : super(key: key);
   final String title;
   _MyLoginPageState createState() => _MyLoginPageState();
 }
-
+//statefulWidget perlu state
+//maka dibawah ini meruppakan state dari kelas MyLoginPage
 class _MyLoginPageState extends State<MyLoginPage> {
   @override
   Widget build(BuildContext context) {
     
+    //field email
     final emailField = TextField(
       obscureText: false,
       style: TextStyle(fontSize: 20.0),
@@ -36,6 +52,7 @@ class _MyLoginPageState extends State<MyLoginPage> {
       ),
     );
 
+    //field password
     final passwordField = TextField(
       obscureText: true,
       style: TextStyle(fontSize: 20.0),
@@ -46,6 +63,7 @@ class _MyLoginPageState extends State<MyLoginPage> {
       ),
     );
 
+    //button login
     final loginButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -66,8 +84,10 @@ class _MyLoginPageState extends State<MyLoginPage> {
       ),
     );
 
+    //pada return value ini merupakan tampilan yang akan muncul pada aplikasi
     return Scaffold(
       body: SingleChildScrollView(
+        //column digunakan untuk menyusun tampilan secara vertical
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
